3c2f0e5a531433b67290df75024a4ce4b5545304
Andreas Madsen <amwebdk@gmail.com>
2012-07-18 22:54:42 +0200

    [test] common now allow createing content from string

--- test/common.js
+++ test/common.js
@@ -19,6 +19,24 @@
       };
 
       body.appendChild(elem);
+    },
+
+    createContent: function (content, callback) {
+      var body = document.getElementsByTagName('body')[0];
+
+      var elem = document.createElement('iframe');
+          elem.style.display = 'none';
+
+      elem.onload = function() {
+        var doc = elem.contentDocument || elem.contentWindow.document;
+        body.removeChild(elem);
+
+        doc.write(content);
+
+        callback(doc);
+      };
+
+      body.appendChild(elem);
     }
   };
 })();
